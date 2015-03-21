e7f3b991ce6cc32994ad37ff9ecaba72b7b72773
Jonas Flesch <contato@jonasflesch.com>
2015-03-21 13:06:44 -0300

    Commit inicial

---
+++ java/ensemble-docking/src/main/java/br/com/jonasflesch/ensemledocking/Application.java
@@ -0,0 +1,16 @@
+package br.com.jonasflesch.ensemledocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by jonasflesch on 3/21/15.
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
